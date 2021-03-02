d6760c09aa7fbf5ce7be430f0e753bf321032218
Hireki <imhireki@gmail.com>
2021-03-02 10:28:05 -0300

    initial commit

---
+++ produto/models.py
@@ -0,0 +1,37 @@
+from PIL import Image
+from django.db import models
+
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        # cria a pasta produto_imagens/ano/mes e opcional com blank e null
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()  # sem default = obrigatorio
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variacao'),
+            ('S', 'Simples')
+        )
+    )
+
+    @staticmethod  # não tem self então static
+    def resize_image(img, new_width=800):
+        print(img.name)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
+    def __str__(self):
+        return self.nome
