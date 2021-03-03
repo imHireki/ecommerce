cea54b9bed6a44bd0ff3101cf5a8330f3903b37b
Hireki <imhireki@gmail.com>
2021-03-03 11:10:35 -0300

    add model Perfil

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
+import re
+from utils.validacpf import valida_cpf
 
-# Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=50)  # correios rua 50
+    # correios max 5, pode incluir letras
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=30)  # correios 30
+    bairro = models.CharField(max_length=30)  # correios 30
+    cep = models.CharField(max_length=8)  # correios 8
+    cidade = models.CharField(max_length=30)  # correios 30
+    estado = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina')
+        )
+    )
+
+    def __str__(self):
+        return f'{self.usuario}'
+
+    
+    def clean(self):
+        error_messages = {}
+        
+        if not valida_cpf(self.cpf):
+            error_messages['cpf'] = 'Digite um CPF Válido!'
+        
+        if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+            error_messages['cep'] = 'CEP inválido, digite os 8 digitos do CEP!'
+        
+        if error_messages:
+            raise ValidationError(error_messages)
+            
+
+    # para a classe não ficar Perfils na area admin
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
