59158daa8eddb557c3927d2a9383b9ffc91106e0
Hireki <imhireki@gmail.com>
2021-03-04 06:41:48 -0300

    add URLs

--- loja/urls.py
+++ loja/urls.py
@@ -20,6 +20,9 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
     # TODO: remover debug toolbar
     path('__debug__/', include(debug_toolbar.urls)),
