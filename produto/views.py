cd1b55ba7a65e51fda943dc00de5453e887932d4
Hireki <imhireki@gmail.com>
2021-03-10 11:22:30 -0300

    add purchase summary

--- produto/views.py
+++ produto/views.py
@@ -170,4 +170,13 @@ class Carrinho(View):
 
 class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('Finalizar')
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+        
+        contexto = {
+            'usuario': self.request.user,
+            'carrinho': self.request.session['carrinho'],
+            
+            
+        }
+        return render(self.request, 'produto/resumodacompra.html', context=contexto)
